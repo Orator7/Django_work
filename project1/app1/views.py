d48d20436e29ffd6e18263c9add878d4565f8630
orator7 <upadhpiyushmp7@gmial.com>
2024-03-07 11:33:10 +0530

    project1

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from app1.models import Topic,Webpage,AccessRecord
 
 # Create your views here.
 
 def home(request):
-    my_dict = {"insert_content":"I am from views of app1"}
-    return render(request,"app1/index.html",context=my_dict)
+    Webpages_list = AccessRecord.objects.order_by('date')
+    date_list ={"insert_content":"I am from views of app1",'access_record':Webpages_list,}
+    
+    my_dict = {}
+    return render(request,"app1/index.html",context=date_list)
